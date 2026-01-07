79c8b8dd24199a5a022abccfe2096748cb156018
Ebba Þóra Hvannberg <ebba@hi.is>
2026-01-07 20:58:10 +0000

    Óþarfa vísbendingar fjarlægðar

--- Vika1/HalloHeimur/src/main/java/is/Main.java
+++ Vika1/HalloHeimur/src/main/java/is/Main.java
@@ -6,8 +6,8 @@ package is;
  *  Lýsing  : Dæmi um einfalt Halló heimur forrit í Java sem keyrir með Maven í IntelliJ
  *
  *
- *****************************************************************************///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ *****************************************************************************/
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
