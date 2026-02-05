1c06413aa9b7f3ad02fb09984f1169cc0ecde0cd
Ebba Þóra Hvannberg <ebba@hi.is>
2026-02-05 11:05:40 +0000

    pom.xml hefur útgáfu 25

---
+++ Vika5/DialogDemoPersonFXML/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.dialogdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    exports com.example.dialogdemo.vidmot;
+    opens com.example.dialogdemo.vidmot to javafx.fxml;
+    exports com.example.dialogdemo.vinnsla;
+    opens com.example.dialogdemo.vinnsla to javafx.fxml;
+}
