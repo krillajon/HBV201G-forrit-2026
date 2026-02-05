44f0ff51575196c99e9ead8aefbd259818dd923b
Ebba Þóra Hvannberg <ebba@hi.is>
2026-02-05 08:49:48 +0000

    pom.xml breytt með útgáfu 25

---
+++ Vika5/TextDialog/src/main/java/is/vidmot/TextDialogController.java
@@ -0,0 +1,40 @@
+
+package is.vidmot;
+
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
+
+
+/**
+ * Sýnidæmi fyrir TextInputDialog 
+ * @author Ebba Þóra Hvannberg ebba@hi.is
+ */
+public class TextDialogController  {
+    /**
+     * Aðferð sem kallað er á eftir að búið er að lesa inn .fxml skrána
+     * Textadialogur búinn til, hann opnaður og fengin gögn úr honum.
+     */
+    public void initialize() {
+        // Textadialog búinn til 
+        TextInputDialog d = new TextInputDialog();
+
+        // Settur titill og haus 
+        d.setTitle("Leikmenn");
+        d.setHeaderText("Hvað heitirðu?");
+        // Einfaldur texti sem meginmál 
+        d.setContentText("Nafn:");
+
+        // Birtum dialog-inn og bíðum og fáum útkomuna
+        // utkoma er af tagi sem inniheldur String 
+        Optional<String> utkoma = d.showAndWait();
+        // Birtum svarið á console með því að nota get()
+        if(utkoma.isPresent()) {
+            System.out.println ("nafnið er "+utkoma.get());
+        }
+        else { // notandi ýtti á cancel
+            System.out.println ("ekkert svar");
+        }
+        d.close();
+    }  
+}
